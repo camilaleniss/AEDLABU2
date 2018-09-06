503f3e37d0b410535d22871fc79e146e2e02a912
camilaleniss <mlmariacami2@gmail.com>
2018-09-05 21:49:14 -0500

    Empezado diseño de soluciones creativas

---
+++ U2-Lenis-Palma-Torres/src/test/TestStackWeapon.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestStackWeapon {
+
+	TestStackWeapon(){
+		
+	}
+	
+	@Test
+	void test() {
+		
+	}
+
+}
