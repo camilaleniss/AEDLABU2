a43ba4de22b9ab6bce1eb82f6faacb7ffb0fd397
Millionhat <38446282+Millionhat@users.noreply.github.com>
2018-09-10 18:54:20 -0500

    Plataformas+ clase jugador

---
+++ U2-Lenis-Palma-Torres/src/model/Mobile.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Mobile implements IQueue<Player> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void enqueue(Player item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Player front() throws QueueException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player dequeue() throws QueueException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ U2-Lenis-Palma-Torres/src/model/Player.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Player {
+	
+	private String nickname;
+	private String platform;
+	
+	public Player(String name, String plat) {
+		nickname=name;
+		platform=plat;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+}
