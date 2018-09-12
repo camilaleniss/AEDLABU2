e8dacb5d045a0cd3bd69cc491654f4cb9204aa16
Millionhat <38446282+Millionhat@users.noreply.github.com>
2018-09-12 16:35:56 -0500

    se implemento el hash en la clase server

--- U2-Lenis-Palma-Torres/src/model/Server.java
+++ U2-Lenis-Palma-Torres/src/model/Server.java
@@ -7,15 +7,34 @@ public class Server {
 		setQueues(new HashTableOpen<String,Queue<Player>>(8));
 		try {
 			queues.insert("Xbox", new Queue<Player>());
+			queues.insert("PlayStation", new Queue<Player>());
+			queues.insert("Switch", new Queue<Player>());
+			queues.insert("Pc", new Queue<Player>());
+			queues.insert("Mobile", new Queue<Player>());
+			queues.insert("All", new Queue<Player>());
 			
 		} catch (HashTableException e) {
 			e.printStackTrace();
 		}
 		
 	}
+	
+	public void addNewPlatform(String platform) {
+		try {
+			queues.insert(platform, new Queue<Player>());
+		} catch (HashTableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addPlayer(Player ready) {
+		queues.search(ready.getPlatform()).enqueue(ready);
+	}
 
 	public HashTableOpen<String,Queue<Player>> getQueues() {
 		return queues;
+		
 	}
 
 	public void setQueues(HashTableOpen<String,Queue<Player>> queues) {
