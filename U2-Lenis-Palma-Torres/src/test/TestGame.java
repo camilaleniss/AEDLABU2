68fbf00639fa61dc6a1a2219489dbaca8706da72
camilaleniss <mlmariacami2@gmail.com>
2018-09-17 10:08:10 -0500

    Escenarios Pruebas Game

--- U2-Lenis-Palma-Torres/src/test/TestGame.java
+++ U2-Lenis-Palma-Torres/src/test/TestGame.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import model.Game;
+import model.Player;
 
 class TestGame {
 
@@ -15,7 +16,46 @@ class TestGame {
 	}
 	
 	void setUpStage1() {
-		
+		Player []  arr = new Player[5];
+		arr[0].setMatchValue(1);
+		arr[0].setMatchValue(2);
+		arr[0].setMatchValue(3);
+		arr[0].setMatchValue(4);
+		arr[0].setMatchValue(10);
 	}
+	
+	void setUpStage2() {
+		Player []  arr = new Player[5];
+		for (int i = 0; i<arr.length; i++)
+			arr[i].setMatchValue(10);
+	}
+	
+	void setUpStage3() {
+		Player []  arr = new Player[5];
+		arr[0].setMatchValue(1);
+		arr[0].setMatchValue(2);
+		arr[0].setMatchValue(3);
+		arr[0].setMatchValue(20);
+		arr[0].setMatchValue(4);
+	}
+	
+	void setUpStage4() {
+		Player []  arr = new Player[5];
+		arr[0].setMatchValue(2);
+		for (int i = 1; i<arr.length-1; i++)
+			arr[i].setMatchValue(1);
+	}
+	
+	void setUpStage5() {
+		Player []  arr = new Player[5];
+		arr[0].setMatchValue(10);
+		arr[0].setMatchValue(15);
+		arr[0].setMatchValue(20);
+		arr[0].setMatchValue(25);
+		arr[0].setMatchValue(30);
+	}
+	
+	
+	
 
 }
