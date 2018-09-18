10d59d464cc08080643643b58a9e828cc43466c6
camilaleniss <mlmariacami2@gmail.com>
2018-09-17 21:59:15 -0500

    Empezada diseños TestMatchmaking

--- U2-Lenis-Palma-Torres - maven/src/main/java/model/Matchmaking.java
+++ U2-Lenis-Palma-Torres - maven/src/main/java/model/Matchmaking.java
@@ -1,20 +1,31 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * This class do the matchmaking of a list of n player
+ * Classifying them in matches, so that the standard deviation has to be the smallest as possible
+ */
 public class Matchmaking {
 
 	private ArrayList<Player> players;
 	private Game[] matches;
 	private int j;
 	private int n;
-
+	/**
+	 * Creates a Matchmaking and initializes the parameters of the several games that can be done by 
+	 * n number of players.
+	 * @param n Number of players in the MatchMaking
+	 */
 	public Matchmaking(int n) {
 		this.n = n;
 		players = new ArrayList<>();
 		int k = (int) Math.ceil((double) n / 100);
 		matches = new Game[k];
 		j = (int) Math.ceil((double) n / k);
+	}
+	
+	public ArrayList<Player> getPlayers(){
+		return players;
 	}
 
 	public boolean addPlayer(Player p) {
@@ -65,8 +76,6 @@ public class Matchmaking {
 		return index;
 	}
 	
-	public ArrayList<Player> getPlayers(){
-		return players;
-	}
+	
 
 }
