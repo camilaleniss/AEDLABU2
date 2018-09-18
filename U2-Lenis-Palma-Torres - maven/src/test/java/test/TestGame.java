f081acab86a2c3ff05cab2082f7917135204d19f
camilaleniss <mlmariacami2@gmail.com>
2018-09-18 10:49:50 -0500

    Update TestGame

--- U2-Lenis-Palma-Torres - maven/src/test/java/test/TestGame.java
+++ U2-Lenis-Palma-Torres - maven/src/test/java/test/TestGame.java
@@ -9,74 +9,130 @@ import model.Player;
 
 class TestGame {
 
-private Game game;
-	
-	TestGame(){
-		game= new Game(5);
+	private Game game;
+
+	TestGame() {
+		game = new Game(5);
 	}
-	
+
 	void setUpStage1() {
-		Player []  arr = new Player[5];
+		Player[] arr = new Player[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new Player("", "");
+		}
 		arr[0].setMatchValue(1);
-		arr[0].setMatchValue(2);
-		arr[0].setMatchValue(3);
-		arr[0].setMatchValue(4);
-		arr[0].setMatchValue(10);
+		arr[1].setMatchValue(2);
+		arr[2].setMatchValue(3);
+		arr[3].setMatchValue(4);
+		arr[4].setMatchValue(10);
 		game.setPlayers(arr);
 	}
-	
+
 	void setUpStage2() {
-		Player []  arr = new Player[5];
-		for (int i = 0; i<arr.length; i++)
+		Player[] arr = new Player[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new Player("", "");
+		}
+		for (int i = 0; i < arr.length; i++)
 			arr[i].setMatchValue(10);
 		game.setPlayers(arr);
 	}
-	
+
 	void setUpStage3() {
-		Player []  arr = new Player[5];
+		Player[] arr = new Player[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new Player("", "");
+		}
 		arr[0].setMatchValue(1);
-		arr[0].setMatchValue(2);
-		arr[0].setMatchValue(3);
-		arr[0].setMatchValue(20);
-		arr[0].setMatchValue(4);
+		arr[1].setMatchValue(2);
+		arr[2].setMatchValue(3);
+		arr[3].setMatchValue(20);
+		arr[4].setMatchValue(4);
 		game.setPlayers(arr);
 	}
-	
+
 	void setUpStage4() {
-		Player []  arr = new Player[5];
+		Player[] arr = new Player[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new Player("", "");
+		}
 		arr[0].setMatchValue(2);
-		for (int i = 1; i<arr.length-1; i++)
+		for (int i = 1; i < arr.length - 1; i++)
 			arr[i].setMatchValue(1);
 		game.setPlayers(arr);
 	}
-	
+
 	void setUpStage5() {
-		Player []  arr = new Player[5];
+		Player[] arr = new Player[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new Player("", "");
+		}
 		arr[0].setMatchValue(10);
-		arr[0].setMatchValue(15);
-		arr[0].setMatchValue(20);
-		arr[0].setMatchValue(25);
-		arr[0].setMatchValue(30);
+		arr[1].setMatchValue(15);
+		arr[2].setMatchValue(20);
+		arr[3].setMatchValue(25);
+		arr[4].setMatchValue(30);
 		game.setPlayers(arr);
 	}
-	
+
 	@Test
 	void testSetPlayers() {
-		setUpStage1();	
-		assertTrue(game.getAtyp()==4);
-		
+		setUpStage1();
+		assertTrue(game.getAtyp() == 4);
+
 		setUpStage2();
-		assertTrue(game.getAtyp()==4);
-		
+		assertTrue(game.getAtyp() == 4);
+
 		setUpStage3();
-		assertTrue(game.getAtyp()==3);
-		
+		assertTrue(game.getAtyp() == 3);
+
 		setUpStage4();
-		assertTrue(game.getAtyp()==0);
-		
+		assertTrue(game.getAtyp() == 4);
+
 		setUpStage5();
-		
-		System.out.print(game.getAtyp());
+		assertTrue(game.getAtyp() == 4);
+
+	}
+
+	@Test
+	void testTryToInsert() {
+		Player ptest;
+
+		Player player = new Player("", "");
+		player.setMatchValue(5);
+
+		setUpStage1();
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 10);
+
+		setUpStage1();
+		player.setMatchValue(11);
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 11);
+
+		setUpStage2();
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 11);
+
+		setUpStage2();
+		player.setMatchValue(9);
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 9);
+
+		setUpStage5();
+		player.setMatchValue(11);
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 30);
+
+		setUpStage5();
+		player.setMatchValue(28);
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 30);
+
+		setUpStage5();
+		player.setMatchValue(40);
+		ptest = game.tryToInsert(player);
+		assertTrue(ptest.getMatchValue() == 40);
 	}
 
 }
