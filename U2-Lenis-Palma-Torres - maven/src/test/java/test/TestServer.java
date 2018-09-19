04d3728053d18872e9f43dd5f10d0a3cbbfb4f94
Millionhat <38446282+Millionhat@users.noreply.github.com>
2018-09-19 17:08:53 -0500

    Se agrego El diagram de Tests y  se Modifico setup
    Modificacion de Setup a SetupStage para mantener uniformidad

--- U2-Lenis-Palma-Torres - maven/src/test/java/test/TestServer.java
+++ U2-Lenis-Palma-Torres - maven/src/test/java/test/TestServer.java
@@ -13,57 +13,57 @@ class TestServer {
 
 	private Server alpha;
 
-	void setup1() {
+	void setUpStage1() {
 		alpha=new Server(10);
 		
 	}
-	void setup2() {
-		setup1();
+	void setUpStage2() {
+		setUpStage1();
 		alpha.addPlayer(new Player("Ninja","Xbox"));
 		alpha.addPlayer(new Player("Rambo", "Switch"));
 	}
-	void setup3() {
-		setup1();
+	void setUpStage3() {
+		setUpStage1();
 		Player logIn= new Player("Jatr9908","Pc");
 		alpha.addPlayer(new Player("Millionhat","Pc"));
 		alpha.addPlayer(new Player("kungFumasta","Pc"));
 		alpha.addPlayer(logIn);
 	}
-	void setup4() {
-		setup1();
+	void setUpStage4() {
+		setUpStage1();
 		alpha.addPlayer(new Player("pinkyFlower","SuperNintendo"));
 	}
-	void setup5() {
-		setup1();
+	void setUpStage5() {
+		setUpStage1();
 		alpha.addNewPlatform("Sega");
 	}
-	void setup6() {
-		setup5();
+	void setUpStage6() {
+		setUpStage5();
 		alpha.addNewPlatform("GameBoy");
 		alpha.addNewPlatform("Failure");
 	}
-	void setup7() {
-		setup1();
+	void setUpStage7() {
+		setUpStage1();
 		alpha.addPlayer(new Player("TripaDeAcero","Atari"));
 	}
 	
 	@Test
 	void testAddNewPlatform() {
-		setup1();
+		setUpStage1();
 		Matchmaking test=alpha.getPlatform().search("Pc");
 		assertTrue(test.getPlayers().size()==0);
 	}
 	
 	@Test
 	void testAddNewPlatform2() {
-		setup5();
+		setUpStage5();
 		assertTrue(alpha.getPlatform().search("Sega")!=null);
 	}
 	
 	@Test
 	void testAddNewPlatform3() {
 		try {
-		setup6();
+		setUpStage6();
 		}catch (Exception e) {
 			assertTrue(e instanceof HashTableException );
 			assertTrue(e.getMessage().equals("Hash table overflow"));
@@ -72,26 +72,26 @@ class TestServer {
 	
 	@Test
 	void testAddPlayer() {
-		setup2();
+		setUpStage2();
 		assertTrue(alpha.getPlatform().search("Xbox").getPlayers().size()==1);
 		assertTrue(alpha.getPlatform().search("Switch").getPlayers().size()==1);
 	}
 	
 	@Test
 	void testAddPlayer2() {
-		setup3();
+		setUpStage3();
 		assertTrue(alpha.getPlatform().search("Pc").getPlayers().size()==3);
 	}
 	
 	@Test
 	void testAddPlayer3() {
 		try {
-		setup4();
+		setUpStage4();
 		}catch (NullPointerException e) {
 			assertTrue(e instanceof NullPointerException);
 		}
 		try {
-			setup7();
+			setUpStage7();
 		}catch (Exception e) {
 			assertTrue(e instanceof NullPointerException);
 		}
@@ -99,21 +99,21 @@ class TestServer {
 	
 	@Test
 	void testDeletePlatform() {
-		setup1();
+		setUpStage1();
 		alpha.deletePlatform("Pc");
 		assertNull(alpha.getPlatform().search("Pc"));
 	}
 	
 	@Test
 	void testDeletePlatform2() {
-		setup5();
+		setUpStage5();
 		alpha.deletePlatform("Sega");
 		assertNull(alpha.getPlatform().search("Sega"));
 	}
 	
 	@Test
 	void testDeletePlatform3() {
-		setup1();
+		setUpStage1();
 		try {
 			alpha.deletePlatform("Sega");
 		}catch (Exception e) {
